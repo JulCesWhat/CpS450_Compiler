2d7ed285346ddc5cabc02c5cb11cc258aa0682fa
julceswhat <julceswhat@gmail.com>
2018-01-19 17:56:53 -0800

    Added multiple file reading.

--- lexer/src/main/java/cps450/Main.java
+++ lexer/src/main/java/cps450/Main.java
@@ -11,24 +11,35 @@ import org.antlr.v4.runtime.Token;
 public class Main
 {
     public static void main(String[] arguments) throws IOException {
-        if(arguments.length != 1)
+        if(arguments.length < 1)
         {
             System.out.println("usage:");
             System.out.println("  lexer <filename>");
+            System.out.println("  lexer -ds <filename>");
             System.exit(1);
         }
 
-        System.out.println();
+        if(arguments[0] == "-ds" && arguments.length > 1) {
+            scanFile(arguments, true, 1);
+        } else {
+            scanFile(arguments, false, 0);
+        }
+    }
 
-        CharStream input = CharStreams.fromFileName(arguments[0]);
-        FloydLexer lexer = new FloydLexer(input);
-        
-        // Read tokens from lexer
-        Token t = lexer.nextToken();
-        while (t.getType() != FloydLexer.EOF) {
-          System.out.println(arguments[0] + ":" + t.getLine() + ":" + t.getText());
-          t = lexer.nextToken();
+    //Does the reading for every files that was inputed
+    public static void scanFile(String[] fileNames, Boolean debug, int position) throws IOException {
+        for(int i = position; i < fileNames.length; i++) {
+            System.out.println();
 
+            CharStream input = CharStreams.fromFileName(fileNames[i]);
+            FloydLexer lexer = new FloydLexer(input);
+            
+            // Read tokens from lexer
+            Token t = lexer.nextToken();
+            while (t.getType() != FloydLexer.EOF) {
+            System.out.println(fileNames[i] + ":" + t.getLine() + ":" + t.getText());
+            t = lexer.nextToken();
+            }
         }
     }
 
